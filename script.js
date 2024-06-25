f092edcf43e911ce2a171d93397f0c8ca6b1a979
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-06-25 14:43:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    const openModalBtn = document.getElementById('openModal');
+    const modal = document.getElementById('myModal');
+    const closeModalBtn = document.querySelector('.close-modal');
+
+    openModalBtn.addEventListener('click', function () {
+        modal.style.display = 'block';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+    closeModalBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+});
+
